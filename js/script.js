3b5267a95707680337166c8a9dedf730c875a2c4
DJ Neill <dj.neill856@gmail.com>
2023-02-19 20:24:50 -0600

    add animation to check guess and insert letter

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ const NUMBER_OF_GUESSES = 6
 let guessesRemaining = NUMBER_OF_GUESSES
 let currentGuess = []
 let nextLetter = 0
-let rightGuessString = WORDS[Math.floor(Math.random()*WORDS.length)]
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 console.log(rightGuessString)
 
 // Build game board of five rows with five boxes
 function initBoard() {
     let board = document.getElementById('gameBoard')
 
-    for(let i = 0; i < NUMBER_OF_GUESSES; i++) {
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
         let row = document.createElement('div')
         row.className = 'letterRow'
 
-        for(let j = 0; j <5; j++) {
+        for (let j = 0; j < 5; j++) {
             let box = document.createElement('div')
             box.className = 'letterBox'
             row.appendChild(box)
@@ -29,12 +29,12 @@ initBoard()
 
 // Keyup event listener for letter keys
 document.addEventListener('keyup', (e) => {
-    if(guessesRemaining === 0) {
+    if (guessesRemaining === 0) {
         return
     }
 
     let pressedKey = String(e.key)
-    if(pressedKey === "Backspace" && nextLetter !==0) {
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
         deleteLetter()
         return
     }
@@ -53,7 +53,7 @@ document.addEventListener('keyup', (e) => {
 })
 
 // Insert letters into gameboard
-function insertLetter (pressedKey) {
+function insertLetter(pressedKey) {
     if (nextLetter === 5) {
         return
     }
@@ -62,6 +62,7 @@ function insertLetter (pressedKey) {
 
     let row = document.getElementsByClassName('letterRow')[6 - guessesRemaining]
     let box = row.children[nextLetter]
+    animateCSS(box, "pulse")
     box.textContent = pressedKey
     box.classList.add('filledBox')
     currentGuess.push(pressedKey)
@@ -69,7 +70,7 @@ function insertLetter (pressedKey) {
 }
 
 // Delete previous letter entered
-function deleteLetter () {
+function deleteLetter() {
     let row = document.getElementsByClassName('letterRow')[6 - guessesRemaining]
     let box = row.children[nextLetter - 1]
     box.textContent = ""
@@ -124,6 +125,8 @@ function checkGuess() {
 
         let delay = 250 * i
         setTimeout(() => {
+            //flip box
+            animateCSS(box, 'flipInX')
             // shade box
             box.style.backgroundColor = letterColor
             shadekeyBoard(letter, letterColor)
@@ -167,7 +170,7 @@ function shadekeyBoard(letter, color) {
 
 // replace notification alerts with toastr
 
-function checkGuess () {
+function checkGuess() {
     let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
     let guessString = ''
     let rightGuess = Array.from(rightGuessString)
@@ -186,12 +189,12 @@ function checkGuess () {
         return
     }
 
-    
+
     for (let i = 0; i < 5; i++) {
         let letterColor = ''
         let box = row.children[i]
         let letter = currentGuess[i]
-        
+
         let letterPosition = rightGuess.indexOf(currentGuess[i])
         // is letter in the correct guess
         if (letterPosition === -1) {
@@ -212,7 +215,7 @@ function checkGuess () {
         }
 
         let delay = 250 * i
-        setTimeout(()=> {
+        setTimeout(() => {
             //shade box
             box.style.backgroundColor = letterColor
             shadeKeyBoard(letter, letterColor)
@@ -239,7 +242,7 @@ function checkGuess () {
 
 document.getElementById('keyboardCont').addEventListener('click', (e) => {
     const target = e.target
-    
+
     if (!target.classList.contains('keyboardButton')) {
         return
     }
@@ -250,27 +253,27 @@ document.getElementById('keyboardCont').addEventListener('click', (e) => {
         key = 'Backspace'
     }
 
-    document.dispatchEvent(new KeyboardEvent('Keyup', {'key': key}))
+    document.dispatchEvent(new KeyboardEvent('Keyup', { 'key': key }))
 })
 
 // Animation for animateCSS
 
 const animateCSS = (element, animation, prefix = 'animate__') =>
-  // We create a Promise and return it
-  new Promise((resolve, reject) => {
-    const animationName = `${prefix}${animation}`;
-    // const node = document.querySelector(element);
-    const node = element
-    node.style.setProperty('--animate-duration', '0.3s');
-    
-    node.classList.add(`${prefix}animated`, animationName);
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        // const node = document.querySelector(element);
+        const node = element
+        node.style.setProperty('--animate-duration', '0.3s');
 
-    // When the animation ends, we clean the classes and resolve the Promise
-    function handleAnimationEnd(event) {
-      event.stopPropagation();
-      node.classList.remove(`${prefix}animated`, animationName);
-      resolve('Animation ended');
-    }
+        node.classList.add(`${prefix}animated`, animationName);
 
-    node.addEventListener('animationend', handleAnimationEnd, {once: true});
-});
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
