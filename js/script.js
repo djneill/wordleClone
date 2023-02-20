3786663d9c9cbae50ca5f5c9df675fa0b4ffc82c
DJ Neill <dj.neill856@gmail.com>
2023-02-19 19:56:36 -0600

    check guess

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ document.addEventListener('keyup', (e) => {
     }
 
     if (pressedKey === 'Enter') {
-        checkGuress()
+        checkGuess()
         return
     }
 
@@ -76,4 +76,72 @@ function deleteLetter () {
     box.classList.remove('filledBox')
     currentGuess.pop()
     nextLetter -= 1
+}
+
+// Check guess
+function checkGuess() {
+    let row = document.getElementsByClassName('letterRow')[6 - guessesRemaining]
+    let guessString = ''
+    let rightGuess = Array.from(rightGuessString)
+
+    for (const val of currentGuess) {
+        guessString += val
+    }
+
+    if (guessString.length != 5) {
+        alert('Not enough letters!')
+        return
+    }
+
+    if (!WORDS.includes(guessString)) {
+        alert('Word no in list!')
+        return
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let letterColor = ''
+        let box = row.children[i]
+        let letter = currentGuess[i]
+
+        let letterPosition = rightGuess.indexOf(currentGuess[i])
+        // is letter in the correct guess
+        if (letterPosition === -1) {
+            letterColor = 'grey'
+        } else {
+            // now, letter is definitely in word
+            // if letter index and right guess index are the same
+            // letter is in the right position 
+            if (currentGuess[i] === rightGuess[i]) {
+                // shade green
+                letterColor = 'green'
+            } else {
+                // shade box yellow
+                letterColor = 'yellow'
+            }
+
+            rightGuess[letterPosition] = '#'
+        }
+
+        let delay = 250 * i
+        setTimeout(() => {
+            // shade box
+            box.style.backgroundColor = letterColor
+            shadekeyBoard(letter, letterColor)
+        }, delay)
+    }
+
+    if (guessString === rightGuessString) {
+        alert('You guessed right! Game Over!')
+        guessesRemaining = 0
+        return
+    } else {
+        guessesRemaining -= 1
+        currentGuess = []
+        nextLetter = 0
+
+        if (guessesRemaining === 0) {
+            alert(`You've run out of guesses! Game Over!`)
+            alert(`The right word was: ${rightGuessString}`)
+        }
+    }
 }
